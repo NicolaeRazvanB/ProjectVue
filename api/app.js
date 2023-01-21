0e74ea754b2d986e2ddd85cf6a4dfb87c6341ce9
NicolaeRazvanB <nicolassky16@gmail.com>
2023-01-21 21:52:34 +0200

    added firebase support

--- api/app.js
+++ api/app.js
@@ -5,13 +5,29 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const jwt = require("jsonwebtoken");
-const logger = require("morgan"); //importing a HTTP logger
+const logger = require("morgan");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(logger("dev")); //using the HTTP logger library
+app.use(logger("dev"));
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+const serviceAccount = require("../api/projectvue-f17cb-firebase-adminsdk-isgck-1dd46ef013.json");
+initializeApp({
+  credential: cert(serviceAccount),
+});
+const dbFirebase = getFirestore();
 
+//REQUESTS
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}!`);
 });
